19911d3f4ac662284615fe9a225b3d979d577ee7
Matthew Montejo <montejododger@yahoo.com>
2023-06-14 07:34:44 -0700

    form css file added

--- tapas_design/frontend/src/App.js
+++ tapas_design/frontend/src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage/LoginFormIndex";
+
 function App() {
-  return (
-    <h1>Hello from App</h1>
-  );
+    return (
+        <Switch>
+            <Route path="/login">
+                <h1>Hello from App</h1>
+                <LoginFormPage />
+            </Route>
+        </Switch>
+    );
 }
 
 export default App;
