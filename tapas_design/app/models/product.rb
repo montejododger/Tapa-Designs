78638dcdcb3ab01b0c049a652ad526b88e9dcc61
Matthew Montejo <montejododger@yahoo.com>
2023-06-20 15:27:04 -0700

    products table migrated and validations done

---
+++ tapas_design/app/models/product.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  name        :string           not null
+#  description :string           not null
+#  price       :integer          not null
+#  category    :string           not null
+#  size        :string           not null
+#  color       :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Product < ApplicationRecord
+
+    validates :name, :description, :price, :category, :presence: true
+    validates :price, numericality: { greater_than: 0 }
+    validates :name, uniqueness: true
+
+
+end
--- tapas_design/db/schema.rb
+++ tapas_design/db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_190550) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_221116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description", null: false
+    t.integer "price", null: false
+    t.string "category", null: false
+    t.string "size", null: false
+    t.string "color", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category"], name: "index_products_on_category", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
