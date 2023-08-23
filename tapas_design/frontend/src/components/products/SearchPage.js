69122ec8b6a07c836744e244821f901e32e2f800
Matthew Montejo <montejododger@yahoo.com>
2023-08-23 10:53:45 -0700

    search no result done

--- tapas_design/frontend/src/components/products/SearchPage.js
+++ tapas_design/frontend/src/components/products/SearchPage.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { createSelector } from "reselect";
 import ProductIndexItem from "./ProductIndexItem";
+import { useParams } from "react-router-dom";
 import "./ProductIndex.css";
 
 const selectProducts = createSelector(
@@ -10,6 +11,7 @@ const selectProducts = createSelector(
 );
 
 function SearchPage() {
+    const { query } = useParams();
     const productsFromState = useSelector(selectProducts);
     const [products, setProducts] = useState([]);
 
@@ -28,7 +30,49 @@ function SearchPage() {
                     ))
                 ) : (
                     <div className="no-items-found">
-                        <span>No items found</span>
+                        <div className="search-loop-head">
+                            <p className="result-count">
+                                No results found for "{query}" found.
+                            </p>
+                        </div>
+                        <div className="no-results-container">
+                            <h4 className="ss-title">Suggestions</h4>
+                            <ul className="sugg-list">
+                                <li>Check for misspellings.</li>
+                                <li>
+                                    Remove possible redundent keywords(ie.
+                                    "products")
+                                </li>
+                                <li>
+                                    Use other words to describe what you are
+                                    searching for.
+                                </li>
+                            </ul>
+                        </div>
+                        <p className="ss-looking">
+                            Still can't find what you're looking for? Contact Us
+                        </p>
+                        <div className="ss-contact">
+                            <h4 className="ss-title">Hours</h4>
+                            <p>
+                                Monday - Thursday: 9:00 AM - 5:00 PM MST <br />{" "}
+                                Friday: 9:00 AM - 2:00 PM MST
+                            </p>
+                        </div>
+                        <div className="ss-phone">
+                            <h4 className="ss-title">
+                                        Phone
+                            </h4>
+                            <p>
+                                <strong>Telephone: </strong> (555) 555 - 5555
+                            </p>
+                        </div>
+                        <div className="ss-email"> 
+                            <h4 className="ss-title">Email</h4>
+                            <p className="email">
+                                notarealemail@gmail.com
+                            </p>
+                        </div>
                     </div>
                 )}
             </section>
